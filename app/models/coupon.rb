defcd6f44afe947f0bb36b661319e7a86e44930d
Ted Chen <teddy70642000@gmail.com>
2020-11-03 18:25:30 +0800

    create uuid

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,15 @@
 class Coupon < ApplicationRecord
   belongs_to :user
+
+  before_create :create_uuid
+ 
+  private
+  def create_uuid
+    self.uuid = serial_generator(10)
+  end
+
+  def serial_generator(n)
+    [*'a'..'z', *'A'..'Z', *0..9].sample(n).join
+  end
+
 end
