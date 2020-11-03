0e69ad079f79f125c2dc53c402f97bc6ee90401d
Ted Chen <teddy70642000@gmail.com>
2020-11-04 01:08:54 +0800

    rspec coupon model validation

--- spec/models/coupon_model_spec.rb
+++ spec/models/coupon_model_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
+require 'taiwanese_id_validator/twid_generator'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.new(
+      email: Faker::Internet.email,
+      password: Faker::String.random
+    )
+  end
+  
+  describe 'Coupon validation' do
+    context 'with name, phone and twid' do
+      it 'should be ok' do
+        coupon = user.build_coupon(
+          name: Faker::Name.name  ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: TwidGenerator.generate
+        )
+        expect(coupon).to be_valid
+      end
+    end
+
+    context 'without name' do
+      it 'should not be ok' do
+        coupon = user.build_coupon(
+          name: "",
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: TwidGenerator.generate
+        )
+        expect(coupon).not_to be_valid
+      end
+    end
+
+    context 'without phone' do
+      it 'should not be ok' do
+        coupon = user.build_coupon(
+          name: Faker::Name.name ,
+          phone: "",
+          twid: TwidGenerator.generate
+        )
+        expect(coupon).not_to be_valid
+      end
+    end
+
+    context 'without twid' do
+      it 'should not be ok' do
+        coupon = user.build_coupon(
+          name: Faker::Name.name ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: ""
+        )
+        expect(coupon).not_to be_valid
+      end
+    end
+
+    context 'with incorrect twid' do
+      it 'should not be ok' do
+        coupon = user.build_coupon(
+          name: Faker::Name.name ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: "fffffff"
+        )
+        expect(coupon).not_to be_valid
+      end
+    end
+  end
 end
